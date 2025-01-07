3a263ae716c2e186a6ccab75c152e8523243b181
Joydip <joydip.mondal@gmail.com>
2025-01-07 13:45:44 +0530

    day-2 upload (task updated)

---
+++ codes/day-2/CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Calculator App...");
+
+            Console.Write("enter first number: ");
+            //string tempFirst = Console.ReadLine();
+            //int firstNumber = int.Parse(tempFirst);
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("enter second number: ");
+            //string tempSecond = Console.ReadLine();
+            //int secondNumber = int.Parse(tempSecond);
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            //calling add method
+            int addResult = Add(firstNumber, secondNumber);
+            //printing result
+            //Console.WriteLine($"Add Result: {addResult}");
+            // Program.PrintResult("Add", addResult);
+            Program.PrintResult(nameof(Add), addResult);
+
+            //calling subtract method
+            int subtractResult = Sub(firstNumber, secondNumber);
+            //printing result
+            //Console.WriteLine($"Subtract Result: {subtractResult}");
+            Program.PrintResult(nameof(Sub), subtractResult);
+
+            //calling Multiply method
+            int multiplyResult = Multiply(firstNumber, secondNumber);
+            //printing result
+            Console.WriteLine($"Multiply Result: {multiplyResult}");
+
+            //calling Divide method
+            int divideResult = Divide(firstNumber, secondNumber);
+            //printing result
+            Console.WriteLine($"Divide Result: {divideResult}");
+        }
+
+        //static int Add(int first, int second)
+        //{
+        //    return first + second;
+        //}
+
+        //if the method has single line of code
+        //new syntax (method body expression syntax)
+        static int Add(int first, int second) => first + second;
+
+        //static int Subtract(int first, int second)
+        //{
+        //    return first - second;
+        //}
+        static int Sub(int first, int second) => first - second;
+
+        //static int Multiply(int first, int second)
+        //{
+        //    return first * second;
+        //}
+        static int Multiply(int first, int second) => first * second;
+
+        //static int Divide(int first, int second)
+        //{
+        //    return first / second;
+        //}
+        static int Divide(int first, int second) => first / second;
+
+        static void PrintResult(string methodName, int result) => Console.WriteLine($"{methodName} result: {result}");
+
+        // ==, !=, >, <, >=, <=
+    }
+}
---
+++ codes/day-2/OperatorsApp/OperatorsApp/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace OperatorsApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int x = 5;
+            int y = 10;
+            int result;
+
+            //result = x & y;
+            //Console.WriteLine($"Bitwise AND: {result}");
+
+            //result = x | y;
+            //Console.WriteLine($"Bitwise OR: {result}");
+
+            //result = x >> 2;
+            //Console.WriteLine($"Bitwise Right Shift: {result}");
+
+            //result = x > y ? x : y;
+            //Console.WriteLine($"Result: {result}");
+
+            if (x > y)
+            {
+                //if()
+                //{ }
+                result = x;
+            }
+            else
+                result = y;
+
+            Console.WriteLine($"Result: {result}");
+
+            Console.Write("enter choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            //if (choice == 1)
+            //{
+
+            //}
+            //else if (choice == 2)
+            //{
+            //}
+            //else
+            //{
+
+            //}
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+
+                default:
+                    Console.WriteLine("something..");
+                    break;
+            }
+        }
+    }
+}
