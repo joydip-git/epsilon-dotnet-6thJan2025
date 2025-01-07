133d59637cf33cc790e0436993d6fe0df910ff2f
Joydip <joydip.mondal@gmail.com>
2025-01-07 11:32:09 +0530

    class and object upload

---
+++ codes/day-2/ClassAndObjectApp/ClassAndObjectApp/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassAndObjectApp
+{
+    class Person
+    {
+        int id;
+        string name;
+        long mobile;
+
+        //no return type
+        //name of this method is same as that of the class
+        //it can accpet arguments (parameterozed ctor) or might not accept any arguments (default/parameterless ctor)
+
+        //default ctor
+        public Person()
+        {
+            //id = 0;
+            //name = null;
+            //mobile = 0;
+        }
+
+        //parameterized ctor
+        public Person(int pId, string pName, long pMobile)
+        {
+            id = pId;
+            name = pName;
+            mobile = pMobile;
+        }
+    }
+}
---
+++ codes/day-2/ClassAndObjectApp/ClassAndObjectApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassAndObjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = SayHello("joydip");
+            Console.WriteLine(message);
+
+            Person joydipPersonRef = AcceptUserInfo();
+            Person karthikPersonef = AcceptUserInfo();
+        }
+
+        static Person AcceptUserInfo()
+        {
+            Console.WriteLine("enter id: ");
+            string tempId = Console.ReadLine(); //1001 -> "1001"
+            int id = int.Parse(tempId);
+
+            Console.WriteLine("enter name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("enter mobile no: ");
+            string tempMob = Console.ReadLine(); //90900... -> "9090..."
+            long mobile = long.Parse(tempMob);
+
+            Person person = new Person(id, name, mobile);
+            return person;
+        }
+
+        static string SayHello(string name)
+        {
+            string welcomeMessage = "Hello " + name;
+            return welcomeMessage;
+        }
+    }
+}
