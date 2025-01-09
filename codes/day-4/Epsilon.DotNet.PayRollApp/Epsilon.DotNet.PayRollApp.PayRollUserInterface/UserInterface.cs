ec8519cdf50b5cc4425a579f386164cddc4e02c1
Joydip <joydip.mondal@gmail.com>
2025-01-09 12:10:41 +0530

    Payroll app completed

---
+++ codes/day-4/Epsilon.DotNet.PayRollApp/Epsilon.DotNet.PayRollApp.PayRollUserInterface/UserInterface.cs
@@ -0,0 +1,14 @@
+﻿using Epsilon.DotNet.PayRollApp.Models;
+using static Epsilon.DotNet.PayRollApp.PayRollUserInterface.Utilities.UIUtility;
+
+namespace Epsilon.DotNet.PayRollApp.PayRollUserInterface
+{
+    class UserInterface
+    {
+        static void Main()
+        {
+            Employee employee = CreateEmployee();
+            Console.WriteLine($"Total Salary of {employee.Name} is {employee.TotalPay}");
+        }       
+    }
+}
---
+++ codes/day-4/Epsilon.DotNet.PayRollApp/Epsilon.DotNet.PayRollApp.PayRollUserInterface/Utilities/UIUtility.cs
@@ -0,0 +1,30 @@
+﻿using Epsilon.DotNet.PayRollApp.Models;
+
+namespace Epsilon.DotNet.PayRollApp.PayRollUserInterface.Utilities
+{
+    static class UIUtility
+    {
+        public static Employee CreateEmployee()
+        {
+            Console.Write("enter id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("enter name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("enter basic: ");
+            decimal basic = decimal.Parse(Console.ReadLine());
+
+            Console.Write("enter da: ");
+            decimal da = decimal.Parse(Console.ReadLine());
+
+            Console.Write("enter hra: ");
+            decimal hra = decimal.Parse(Console.ReadLine());
+
+            //Employee employee = new Employee(id, name, basic, da, hra);
+            Employee employee = new(id, name, basic, da, hra);
+            employee.CalculateSalary();
+            return employee;
+        }
+    }
+}
