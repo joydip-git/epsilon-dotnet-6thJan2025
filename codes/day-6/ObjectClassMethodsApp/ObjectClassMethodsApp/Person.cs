d283145b66b8109edab870f1c510c2bf35737208
Joydip <joydip.mondal@gmail.com>
2025-01-13 11:57:21 +0530

    Object class methods

---
+++ codes/day-6/ObjectClassMethodsApp/ObjectClassMethodsApp/Person.cs
@@ -0,0 +1,80 @@
+﻿namespace ObjectClassMethodsApp
+{
+    internal class Person //:Object
+    {
+        int id;
+        string name = string.Empty;
+        decimal salary;
+
+        public Person()
+        {
+
+        }
+
+        public Person(int id, string name, decimal salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public decimal Salary { get => salary; set => salary = value; }
+
+        //you are providing a new implementation of the ToString() method of base class, Object, and you want the child class method to be invoked always
+        public override string ToString()
+        {
+            return $"Id:{this.id},Name:{this.name},Salary:{this.salary}";
+
+            //base class ToString() method code
+            //return this.GetType().FullName;
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            int hash = this.id.GetHashCode() * prime;
+            hash = this.name.GetHashCode() * hash;
+            hash = this.salary.GetHashCode() * hash;
+            return hash;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            //whether the null reference has been passed or not
+            if (obj == null) return false;
+
+            //whether both reference variable (this and obj) are referring to the same instance or not
+            if (this == obj) return true;
+
+            //whether the data type of object is Person or not
+            //if (!(obj is Person)) return false;
+
+            //equivalent to this following code:
+            //if (obj.GetType()!= typeof(Person)) return false;
+
+            //using pattern matching
+            if (obj is not Person) return false;
+
+            //down-casting (taking the reference of the actual (child) class object back to its reference variable from base class reference variable)
+            Person other = (Person)obj;
+            if (this.id != other.id) return false;
+
+            if (this.name != other.name) return false;
+
+            if (this.salary != other.salary) return false;
+
+            return true;
+        }
+
+        public static bool operator == (Person a, Person b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator !=(Person a, Person b)
+        {
+            return !a.Equals(b);
+        }
+    }
+}
---
+++ codes/day-6/ObjectClassMethodsApp/ObjectClassMethodsApp/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ObjectClassMethodsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person anilPerson = new(1, "anil", 1000);
+            //Console.WriteLine($"Id:{anilPerson.Id},Name:{anilPerson.Name},Salary:{anilPerson.Salary}");
+            //Console.WriteLine(anilPerson.ToString());
+
+            //when printing the reference, actually ToString() gets invoked by default
+            //if you have NOT overriden the ToString() method, then the base class (Object) ToString() method will be invoked, which will print the data type of this object (along with namespace name)
+            //output => ObjectClassMethodsApp.Person
+            Console.WriteLine(anilPerson);
+
+            //GetType() method returns an instance of another class, known as Type, storing information about Person class (such as: fully qualified name [name of the type along with namespace name], name (type name), which are the data members, methods, constructors, properties etc. written inside the class etc.)
+            Type personType = anilPerson.GetType();
+            Console.WriteLine($"Name:{personType.Name}");
+            Console.WriteLine($"Fully Qualified Name:{personType.FullName}");
+
+            //anoter way to extract type information (using typeof operator)
+            //Type personClsType = typeof(Person);
+            //Console.WriteLine($"Name:{personClsType.Name}");
+            //Console.WriteLine($"Fully Qualified Name:{personClsType.FullName}");
+
+            //Person sunilPerson = anilPerson;
+            Person sunilPerson = new(1, "anil", 1000);
+
+            //Person joydipPerson = sunilPerson;
+            //Person joydipPerson = new(3, "joydip", 3000);
+
+            // anilPerson -> current instance
+            // sunilPerson -> other instance
+
+            if (anilPerson.GetHashCode() == sunilPerson.GetHashCode())
+            {
+                if (anilPerson == sunilPerson)
+                //if (anilPerson.Equals(sunilPerson))
+                {
+                    Console.WriteLine("same");
+                }
+                else
+                    Console.WriteLine("not same");
+            }
+            else
+                Console.WriteLine("not same");
+        }
+    }
+}
