27ec03b43e3c15b3dbdd2da598e6edf19e64f541
Joydip <joydip.mondal@gmail.com>
2025-01-22 13:54:29 +0530

    Day-12 upload: LINQ and Threads, Task completed

---
+++ codes/day-12/ThreadingDemo/TaskApp/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace TaskApp
+{
+    internal class Program
+    {
+        //static void Main()
+        static async Task Main()
+        {
+            Console.WriteLine($"Main Thread: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("\n");
+            try
+            {
+                double result = await PerformNumberTask();
+                Console.WriteLine($"Task Result: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            //Task<double> numberTask = PerformNumberTask();
+
+            //Result property is a blocking property => it blocks the remaining code in this scope until and unless the Task is over
+            //Console.WriteLine($"Task Result: {numberTask.Result}");
+
+            //Task<double> numberTask = PerformNumberTask();
+            //if (numberTask.Status == TaskStatus.RanToCompletion)
+            //{
+            //    Console.WriteLine($"Task Result: {numberTask.Result}");
+            //}
+            //else
+            //    Console.WriteLine(numberTask.Status);
+
+            //bool isOver = numberTask.Wait(1500);
+            //if (isOver)
+            //{
+            //    Console.WriteLine($"Task Result: {numberTask.Result}");
+            //}
+            //else
+            //    Console.WriteLine("task is not over yet...");
+            Console.WriteLine("This is the last line of Main Method...");
+        }
+        static Task<double> PerformNumberTask()
+        {
+            Task<double> numberTask = Task.Run(
+                () =>
+                {
+                    Console.WriteLine($"Task Thread: {Thread.CurrentThread.ManagedThreadId}");
+                    int[] ints = [1, 2, 3, 4];
+                    Thread.Sleep(2000);
+                    return ints.Average();
+                }
+                );
+            return numberTask;
+        }
+    }
+}
---
+++ codes/day-12/ThreadingDemo/ThreadingDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ThreadingDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"In Main Method. Thread => {Thread.CurrentThread.ManagedThreadId}");
+
+            //thread for for Run Method
+            //ThreadStart runDelegate = new(Run);
+            //Thread runThread = new(runDelegate);
+
+            //or shortcut
+            Thread runThread = new(Run);
+
+            //thread for for RunAnother Method
+            //ParameterizedThreadStart runAnotherDelegate = new(RunAnother);
+            //Thread runAnotherThread = new(runAnotherDelegate);
+
+            //or shortcut
+            Thread runAnotherThread = new(RunAnother);
+
+            //Run();
+            //RunAnother(5);
+
+            runThread.Start();
+            runThread.Join();
+
+            runAnotherThread.Start(5);
+            runAnotherThread.Join();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Main: {i + 1}");
+            }
+        }
+        static void Run()
+        {
+            Console.WriteLine($"In Run Method. Thread => {Thread.CurrentThread.ManagedThreadId}");
+
+            Thread.Sleep(1000);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Run: {i + 1}");
+            }
+        }
+        static void RunAnother(object? obj)
+        {
+            Console.WriteLine($"In Run Another Method. Thread => {Thread.CurrentThread.ManagedThreadId}");
+            int count = (int)obj;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"RunAnother: {i + 1}");
+            }
+        }
+    }
+}
